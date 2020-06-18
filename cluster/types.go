557cc3887956917221e52b358728e5a1cf68f61e
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-06-18 20:14:06 +0200

    add multinode structure

--- cluster/types.go
+++ cluster/types.go
@@ -18,6 +18,9 @@ type Node interface {
 	Ready() <-chan struct{}
 	Call(id uint64, f func(*grpc.ClientConn) error) error
 }
+type MultiNode interface {
+	Node(cluster string) Node
+}
 
 type NodeConfig struct {
 	ID               uint64
