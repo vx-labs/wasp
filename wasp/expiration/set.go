869257c980882fc5e2b44f51a9821576fd163b12
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-11-11 22:54:49 +0100

    add expiration package

---
+++ wasp/expiration/set.go
@@ -0,0 +1,91 @@
+package expiration
+
+import (
+	"sync"
+	"time"
+
+	"github.com/google/btree"
+)
+
+type bucket struct {
+	data     map[int]struct{}
+	deadline time.Time
+}
+
+func (e *bucket) Less(b btree.Item) bool {
+	return e.deadline.Before(b.(*bucket).deadline)
+}
+
+type List interface {
+	Insert(id int, deadline time.Time)
+	Delete(id int, deadline time.Time)
+	Update(id int, old time.Time, new time.Time)
+	Expire(now time.Time) []int
+}
+
+type list struct {
+	mtx  sync.Mutex
+	tree *btree.BTree
+}
+
+func NewList() *list {
+	return &list{tree: btree.New(2)}
+}
+
+func (l *list) Insert(id int, deadline time.Time) {
+	l.mtx.Lock()
+	defer l.mtx.Unlock()
+	l.insert(id, deadline)
+}
+
+func (l *list) Delete(id int, deadline time.Time) {
+	l.delete(id, deadline)
+}
+func (l *list) delete(id int, deadline time.Time) {
+	set := l.tree.Get(&bucket{deadline: deadline.Round(time.Second)})
+	if set != nil {
+		delete(set.(*bucket).data, id)
+		if len(set.(*bucket).data) == 0 {
+			l.tree.Delete(&bucket{deadline: deadline.Round(time.Second)})
+		}
+	}
+}
+func (l *list) insert(id int, deadline time.Time) {
+	set := l.tree.Get(&bucket{deadline: deadline.Round(time.Second)})
+	if set == nil {
+		set = &bucket{data: make(map[int]struct{}), deadline: deadline.Round(time.Second)}
+	}
+	set.(*bucket).data[id] = struct{}{}
+	l.tree.ReplaceOrInsert(set)
+}
+
+func (l *list) Update(id int, old time.Time, new time.Time) {
+	l.mtx.Lock()
+	defer l.mtx.Unlock()
+	l.delete(id, old)
+	set := l.tree.Get(&bucket{deadline: new.Round(time.Second)})
+	if set == nil {
+		set = &bucket{data: make(map[int]struct{}), deadline: new.Round(time.Second)}
+	}
+	set.(*bucket).data[id] = struct{}{}
+	l.tree.ReplaceOrInsert(set)
+}
+
+func (l *list) Expire(now time.Time) []int {
+	l.mtx.Lock()
+	defer l.mtx.Unlock()
+	deleted := []time.Time{}
+	out := []int{}
+	l.tree.AscendLessThan(&bucket{deadline: now}, func(i btree.Item) bool {
+		set := i.(*bucket)
+		for id := range set.data {
+			out = append(out, id)
+		}
+		deleted = append(deleted, set.deadline)
+		return true
+	})
+	for _, t := range deleted {
+		l.tree.Delete(&bucket{deadline: t})
+	}
+	return out
+}
---
+++ wasp/expiration/set_test.go
@@ -0,0 +1,38 @@
+package expiration
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestList(t *testing.T) {
+	t.Run("should keep non expired item", func(t *testing.T) {
+		l := NewList()
+		l.Insert(1, time.Time{})
+		expired := l.Expire(time.Time{})
+		require.Equal(t, 0, len(expired))
+	})
+	t.Run("should remove expired items", func(t *testing.T) {
+		l := NewList()
+		l.Insert(1, time.Time{})
+		expired := l.Expire(time.Time{}.Add(1 * time.Minute))
+		require.Equal(t, 1, len(expired))
+		require.Contains(t, expired, 1)
+	})
+	t.Run("should allow item renewal", func(t *testing.T) {
+		l := NewList()
+		l.Insert(1, time.Time{})
+		l.Update(1, time.Time{}, time.Time{}.Add(2*time.Minute))
+		expired := l.Expire(time.Time{}.Add(1 * time.Minute))
+		require.Equal(t, 0, len(expired))
+	})
+	t.Run("should allow item deletion", func(t *testing.T) {
+		l := NewList()
+		l.Insert(1, time.Time{})
+		l.Delete(1, time.Time{})
+		expired := l.Expire(time.Time{}.Add(1 * time.Minute))
+		require.Equal(t, 0, len(expired))
+	})
+}
