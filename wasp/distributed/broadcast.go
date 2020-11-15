f16ad0bc354ca430754a4e474e921caaa9c3056e
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-11-15 17:30:55 +0100

    distributed: add topics state

---
+++ wasp/distributed/broadcast.go
@@ -0,0 +1,15 @@
+package distributed
+
+import "github.com/hashicorp/memberlist"
+
+type simpleBroadcast []byte
+
+func (b simpleBroadcast) Message() []byte                       { return []byte(b) }
+func (b simpleBroadcast) Invalidates(memberlist.Broadcast) bool { return false }
+func (b simpleBroadcast) Finished()                             {}
+
+type simpleFullStateBroadcast []byte
+
+func (b simpleFullStateBroadcast) Message() []byte                       { return []byte(b) }
+func (b simpleFullStateBroadcast) Invalidates(memberlist.Broadcast) bool { return true }
+func (b simpleFullStateBroadcast) Finished()                             {}
---
+++ wasp/distributed/topics.go
@@ -0,0 +1,185 @@
+package distributed
+
+import (
+	"sync"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/hashicorp/memberlist"
+	"github.com/vx-labs/mqtt-protocol/packet"
+	"github.com/vx-labs/wasp/crdt"
+	"github.com/vx-labs/wasp/topics"
+	"github.com/vx-labs/wasp/wasp/api"
+)
+
+type topicsState struct {
+	mu    sync.RWMutex
+	tree  topics.Store
+	bcast *memberlist.TransmitLimitedQueue
+}
+
+func newTopicState(bcast *memberlist.TransmitLimitedQueue) TopicsState {
+	return &topicsState{
+		tree:  topics.NewTree(),
+		bcast: bcast,
+	}
+}
+func (t *topicsState) mergeMessage(msg *api.RetainedMessage) error {
+	if msg.Publish == nil || len(msg.Publish.Topic) == 0 {
+		return ErrInvalidPayload
+	}
+
+	localList, err := t.get(msg.Publish.Topic)
+	if err != nil {
+		return err
+	}
+	if len(localList) > 1 {
+		return ErrInvalidPayload
+	}
+	outdated := true
+	if len(localList) == 1 {
+		outdated = crdt.IsEntryOutdated(localList[1], msg)
+	}
+	if outdated {
+		if crdt.IsEntryAdded(msg) {
+			err = t.set(msg.Publish.Topic, msg)
+			if err != nil {
+				return err
+			}
+		} else if crdt.IsEntryRemoved(msg) {
+			err = t.delete(msg.Publish.Topic, msg)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+func (t *topicsState) GetBroadcasts(overhead, limit int) [][]byte {
+	return t.bcast.GetBroadcasts(overhead, limit)
+}
+
+func (t *topicsState) Merge(buf []byte) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	payload := &api.StateBroadcastEvent{}
+	err := proto.Unmarshal(buf, payload)
+	if err != nil {
+		return err
+	}
+	for _, msg := range payload.RetainedMessages {
+		err := t.mergeMessage(msg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (t *topicsState) Set(message *packet.Publish) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	msg := &api.RetainedMessage{
+		Publish:   message,
+		LastAdded: clock(),
+	}
+	err := t.set(message.Topic, msg)
+	if err != nil {
+		return err
+	}
+	buf, err := proto.Marshal(&api.StateBroadcastEvent{
+		RetainedMessages: []*api.RetainedMessage{msg},
+	})
+	if err != nil {
+		return err
+	}
+	t.bcast.QueueBroadcast(simpleBroadcast(buf))
+	return nil
+}
+func (t *topicsState) set(topic []byte, msg *api.RetainedMessage) error {
+	buf, err := proto.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	err = t.tree.Insert(topic, buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *topicsState) Delete(topic []byte) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	msg := &api.RetainedMessage{
+		Publish: &packet.Publish{
+			Header:  &packet.Header{},
+			Topic:   topic,
+			Payload: nil,
+		},
+		LastDeleted: clock(),
+	}
+	err := t.delete(topic, msg)
+	if err != nil {
+		return err
+	}
+	buf, err := proto.Marshal(&api.StateBroadcastEvent{
+		RetainedMessages: []*api.RetainedMessage{msg},
+	})
+	if err != nil {
+		return err
+	}
+	t.bcast.QueueBroadcast(simpleBroadcast(buf))
+	return nil
+}
+
+func (t *topicsState) delete(topic []byte, msg *api.RetainedMessage) error {
+	buf, err := proto.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	err = t.tree.Insert(topic, buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *topicsState) Get(pattern []byte) ([]*packet.Publish, error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	v, err := t.get(pattern)
+	if err != nil {
+		return nil, err
+	}
+	c := 0
+	for idx := range v {
+		if crdt.IsEntryAdded(v[idx]) {
+			c++
+		}
+	}
+	out := make([]*packet.Publish, c)
+
+	idx := 0
+	for _, msg := range v {
+		if crdt.IsEntryAdded(v[idx]) {
+			out[idx] = msg.Publish
+			idx++
+		}
+	}
+	return out, nil
+}
+func (t *topicsState) get(pattern []byte) ([]*api.RetainedMessage, error) {
+	outBuf := make([][]byte, 0)
+	err := t.tree.Match(pattern, &outBuf)
+	if err != nil {
+		return nil, err
+	}
+	out := make([]*api.RetainedMessage, len(outBuf))
+	for idx := range outBuf {
+		err = proto.Unmarshal(outBuf[idx], out[idx])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return out, nil
+}
