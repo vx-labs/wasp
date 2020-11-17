1cecc6b34719ed69e9dd7b5294e57dda86c7a524
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-11-17 23:56:11 +0100

    local state: fallback to a locked map

--- wasp/state.go
+++ wasp/state.go
@@ -1,6 +1,8 @@
 package wasp
 
 import (
+	"sync"
+
 	"github.com/vx-labs/wasp/wasp/sessions"
 	"github.com/zond/gotomic"
 )
@@ -18,10 +20,14 @@ type hashmapState struct {
 }
 
 func NewState(id uint64) LocalState {
-	return &hashmapState{
-		id:       id,
-		sessions: gotomic.NewHash(),
-	}
+	return newLockedMapState(id)
+}
+
+func newHashmapState(id uint64) LocalState {
+	return &hashmapState{id: id, sessions: gotomic.NewHash()}
+}
+func newLockedMapState(id uint64) LocalState {
+	return &lockedMapState{id: id, sessions: map[string]*sessions.Session{}}
 }
 
 func (s *hashmapState) Get(id string) *sessions.Session {
@@ -52,4 +58,41 @@ func (s *hashmapState) Delete(id string) *sessions.Session {
 		return old.(*sessions.Session)
 	}
 	return nil
+}
+
+type lockedMapState struct {
+	id       uint64
+	sessions map[string]*sessions.Session
+	mtx      sync.RWMutex
+}
+
+func (s *lockedMapState) Get(id string) *sessions.Session {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+	return s.sessions[id]
+}
+func (s *lockedMapState) ListSessions() []*sessions.Session {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+
+	out := []*sessions.Session{}
+	for _, s := range s.sessions {
+		out = append(out, s)
+	}
+	return out
+}
+func (s *lockedMapState) Create(id string, session *sessions.Session) *sessions.Session {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	old := s.sessions[id]
+	s.sessions[id] = session
+	return old
+}
+func (s *lockedMapState) Delete(id string) *sessions.Session {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	old := s.sessions[id]
+	delete(s.sessions, id)
+	return old
 }
--- wasp/state_test.go
+++ wasp/state_test.go
@@ -5,12 +5,13 @@ import (
 	"testing"
 
 	"github.com/vx-labs/wasp/wasp/sessions"
+	"github.com/zond/gotomic"
 )
 
-func benchmarkFor(n int) func(b *testing.B) {
+func benchmarkFor(st func() LocalState, n int) func(b *testing.B) {
 	return func(b *testing.B) {
 		b.StopTimer()
-		s := NewState(1)
+		s := st()
 		for i := 0; i < n; i++ {
 			s.Create(fmt.Sprintf("%d", i), &sessions.Session{})
 		}
@@ -21,10 +22,10 @@ func benchmarkFor(n int) func(b *testing.B) {
 		}
 	}
 }
-func benchmarkParallelFor(n int) func(b *testing.B) {
+func benchmarkParallelFor(st func() LocalState, n int) func(b *testing.B) {
 	return func(b *testing.B) {
 		b.StopTimer()
-		s := NewState(1)
+		s := st()
 		for i := 0; i < n; i++ {
 			s.Create(fmt.Sprintf("%d", i), &sessions.Session{})
 		}
@@ -39,11 +40,23 @@ func benchmarkParallelFor(n int) func(b *testing.B) {
 }
 func BenchmarkState(b *testing.B) {
 	b.Run("get", func(b *testing.B) {
-		b.Run("100", benchmarkFor(100))
-		b.Run("100P", benchmarkParallelFor(100))
-		b.Run("10000", benchmarkFor(10000))
-		b.Run("10000P", benchmarkParallelFor(10000))
-		b.Run("1000000", benchmarkFor(10000))
-		b.Run("1000000P", benchmarkParallelFor(10000))
+		b.Run("hashmap", func(b *testing.B) {
+			prov := func() LocalState { return &hashmapState{sessions: gotomic.NewHash()} }
+			b.Run("100", benchmarkFor(prov, 100))
+			b.Run("100P", benchmarkParallelFor(prov, 100))
+			b.Run("10000", benchmarkFor(prov, 10000))
+			b.Run("10000P", benchmarkParallelFor(prov, 10000))
+			b.Run("1000000", benchmarkFor(prov, 10000))
+			b.Run("1000000P", benchmarkParallelFor(prov, 10000))
+		})
+		b.Run("lockedMap", func(b *testing.B) {
+			prov := func() LocalState { return &lockedMapState{sessions: map[string]*sessions.Session{}} }
+			b.Run("100", benchmarkFor(prov, 100))
+			b.Run("100P", benchmarkParallelFor(prov, 100))
+			b.Run("10000", benchmarkFor(prov, 10000))
+			b.Run("10000P", benchmarkParallelFor(prov, 10000))
+			b.Run("1000000", benchmarkFor(prov, 10000))
+			b.Run("1000000P", benchmarkParallelFor(prov, 10000))
+		})
 	})
 }
