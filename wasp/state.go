a30b9cf4e39b669ac2e948289fd791ecc47092e2
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-11-18 20:05:41 +0100

    local: remove hashmap state

--- wasp/state.go
+++ wasp/state.go
@@ -23,41 +23,8 @@ func NewState(id uint64) LocalState {
 	return newLockedMapState(id)
 }
 
-func newHashmapState(id uint64) LocalState {
-	return &hashmapState{id: id, sessions: gotomic.NewHash()}
-}
 func newLockedMapState(id uint64) LocalState {
 	return &lockedMapState{id: id, sessions: map[string]*sessions.Session{}}
-}
-
-func (s *hashmapState) Get(id string) *sessions.Session {
-	v, ok := s.sessions.Get(gotomic.StringKey(id))
-	if !ok {
-		return nil
-	}
-	return v.(*sessions.Session)
-}
-func (s *hashmapState) ListSessions() []*sessions.Session {
-	out := []*sessions.Session{}
-	s.sessions.Each(func(k gotomic.Hashable, v gotomic.Thing) bool {
-		out = append(out, v.(*sessions.Session))
-		return false
-	})
-	return out
-}
-func (s *hashmapState) Create(id string, session *sessions.Session) *sessions.Session {
-	old, ok := s.sessions.Put(gotomic.StringKey(id), session)
-	if ok {
-		return old.(*sessions.Session)
-	}
-	return nil
-}
-func (s *hashmapState) Delete(id string) *sessions.Session {
-	old, ok := s.sessions.Delete(gotomic.StringKey(id))
-	if ok {
-		return old.(*sessions.Session)
-	}
-	return nil
 }
 
 type lockedMapState struct {
--- wasp/state_test.go
+++ wasp/state_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/vx-labs/wasp/wasp/sessions"
-	"github.com/zond/gotomic"
 )
 
 func benchmarkFor(st func() LocalState, n int) func(b *testing.B) {
@@ -44,15 +43,6 @@ func benchmarkParallelFor(st func() LocalState, n int) func(b *testing.B) {
 func BenchmarkState(b *testing.B) {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	b.Run("get", func(b *testing.B) {
-		b.Run("hashmap", func(b *testing.B) {
-			prov := func() LocalState { return &hashmapState{sessions: gotomic.NewHash()} }
-			b.Run("100", benchmarkFor(prov, 100))
-			b.Run("100P", benchmarkParallelFor(prov, 100))
-			b.Run("10000", benchmarkFor(prov, 10000))
-			b.Run("10000P", benchmarkParallelFor(prov, 10000))
-			b.Run("1000000", benchmarkFor(prov, 10000))
-			b.Run("1000000P", benchmarkParallelFor(prov, 10000))
-		})
 		b.Run("lockedMap", func(b *testing.B) {
 			prov := func() LocalState { return &lockedMapState{sessions: map[string]*sessions.Session{}} }
 			b.Run("100", benchmarkFor(prov, 100))
